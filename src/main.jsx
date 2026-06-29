3ac956acbb0871675f81f1924d56132599529d9e
radhika817 <radhikakandalkar54@gmail.com>
2026-06-30 00:47:08 +0530

    replace App component with MyApp and add workspace configuration

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,16 @@ import { createRoot } from 'react-dom/client'
 
 import App from './App.jsx'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+
+function MyApp(){
+  return(
+    <div>
+      <h1>Custom App</h1>
+    </div>
+  )
+}
+ReactDOM.createRoot(document.getElementById('root')).render(
+ 
+    <MyApp />
+ 
 )
